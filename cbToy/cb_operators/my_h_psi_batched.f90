81fa65c66660564f3f0325a15ad89783eaef056c
Pietro Delugas <mail@pietrodelugas.it>
2026-02-05 14:45:00 +0100

    more asyncs in the main program

--- cbToy/cb_operators/my_h_psi_batched.f90
+++ cbToy/cb_operators/my_h_psi_batched.f90
@@ -7,12 +7,12 @@ subroutine my_h_psi_batched(npwx, npw, nbnd, psi, hpsi, i_batch)
   complex(dp) :: psi(npwx, nbnd), hpsi(npwx, nbnd) 
   !
   integer :: ig, ibnd, ir
-  !$acc data present(psi, hpsi) 
+  !$acc data present(psi, hpsi) async(i_batch)
   do ibnd =1, nbnd
-    !$acc kernels 
+    !$acc kernels async(i_batch)
     fft_array_batched(:,i_batch)=cmplx(0._dp, 0._dp, kind=dp)
     !$acc end kernels 
-    !$acc parallel loop 
+    !$acc parallel loop async(i_batch)
     do ig = 1, npw
       ! if (igk_batched(ig,i_batch) <= 0 .or. igk_batched(ig,i_batch) > size(dfft%nl)) stop "igk_batched index out of range"
       fft_array_batched(dfft%nl(igk_batched(ig,i_batch)),i_batch) = psi(ig,ibnd) 
@@ -20,14 +20,14 @@ subroutine my_h_psi_batched(npwx, npw, nbnd, psi, hpsi, i_batch)
     !$acc host_data use_device(fft_array_batched) 
     call invfft( 'Wave', fft_array_batched(:,i_batch), dfft) 
     !$acc end host_data 
-    !$acc parallel loop 
+    !$acc parallel loop async(i_batch)
     do ir = 1, dfft%nnr 
        fft_array_batched(ir, i_batch) = fft_array_batched(ir,i_batch) * vloc(ir) 
     end do 
     !$acc host_data use_device(fft_array_batched) 
     call fwfft('Wave', fft_array_batched(:,i_batch), dfft) 
     !$acc end host_data
-    !$acc parallel loop 
+    !$acc parallel loop async(i_batch)
     do ig = 1, npw
        hpsi(ig,ibnd) = ekin_batched(ig,i_batch) * psi(ig,ibnd)   + &
                        fft_array_batched(dfft%nl(igk_batched(ig,i_batch)),i_batch)  
